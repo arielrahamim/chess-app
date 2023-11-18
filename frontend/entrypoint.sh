cef211a07a2d9a03de828bf460fabe63e5b920ad
arielrahamim <ariel_rahamim@yahoo.com>
2023-11-18 15:54:03 +0200

    github actions proggress

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 export REACT_APP_POD=$HOSTNAME
-export REACT_APP_HOST="$(TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` && curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/public-ipv4):30050"
 
 npm run build && serve -s build
